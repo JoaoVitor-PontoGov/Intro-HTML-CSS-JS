dd34395d960a791ac59cb95f9a23cfa4556831f0
JoaoVitor-PontoGov <joaovitor@pontogovsistemas.com.br>
2025-08-05 09:35:30 -0300

    feat: Criação do sistema de alteração no nome do visitante

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const botao = document.getElementById("botao");
 
+
 	botao.addEventListener("click", function () {
 		const descricao = document.getElementById("descricao");
 
 		descricao.innerHTML = "<p>Eu <strong>tento</strong> bastante</p>";
 	})
+
+	const botaoVisitante = document.getElementById("botaoVisitante");
+	botaoVisitante.addEventListener("click", function () {
+		const nome = document.getElementById("nome").value;
+		const olaVisitante = document.getElementById("olaVisitante");
+
+		olaVisitante.innerHTML = "<h2>Olá " + nome + "</h2>";
+	})
+
 })
+
