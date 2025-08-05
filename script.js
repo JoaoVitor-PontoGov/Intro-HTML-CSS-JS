f68d4d263e6d02e13aee3799dc64e17112d036d7
JoaoVitor-PontoGov <joaovitor@pontogovsistemas.com.br>
2025-08-05 11:03:53 -0300

    feat: Criação da tabela de hobbies, estilizada e funcional

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	const botao = document.getElementById("botao");
 	const botaoVisitante = document.getElementById("botaoVisitante");
 	const modoDark = document.getElementById("modoDark");
+	const botaoHobbie = document.getElementById("botaoHobbie");
+	const nome = document.getElementById("nome");
+	const hobbie = document.getElementById("campoHobbie")
 
 	botao.addEventListener("click", function () {
 		const descricao = document.getElementById("descricao");
@@ -10,14 +13,29 @@ document.addEventListener("DOMContentLoaded", function () {
 	})
 
 	botaoVisitante.addEventListener("click", function () {
-		const nome = document.getElementById("nome").value;
-		const olaVisitante = document.getElementById("olaVisitante");
+		if (nome.value == "") {
+		} else {
+			const olaVisitante = document.getElementById("olaVisitante");
+			olaVisitante.innerHTML = "<h2>Olá " + nome.value + "</h2>";
+		}
 
-		olaVisitante.innerHTML = "<h2>Olá " + nome + "</h2>";
 	})
 
 	modoDark.addEventListener("click", function () {
 		document.body.classList.toggle("dark")
+	})
+
+	botaoHobbie.addEventListener("click", function () {
+		if (hobbie.value == "") {
+		} else {
+			const hobbie = document.getElementById("campoHobbie")
+			const tabela = document.getElementById("tabelaHobbie");
+			const novalinha = document.createElement("tr");
+			const novaData = document.createElement("td");
+			novalinha.append(novaData)
+			novaData.textContent = hobbie.value;
+			tabela.append(novalinha);
+		}
 	})
 })
 
